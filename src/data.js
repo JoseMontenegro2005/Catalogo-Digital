17ca520f0630d4f0ac082bc25c7deb8b675e1dfc
JoseMontenegro2005 <josemontenegro@unicomfacauca.edu.co>
2025-10-15 22:35:20 -0500

    Actualicé los PDFs de belleza y eliminé los antiguos

--- src/data.js
+++ src/data.js
@@ -4,108 +4,101 @@ export const catalogos = [
     nombre: 'Arrurru',
     categoria: ['belleza'], 
     archivosPdf: [ 
-    { titulo: 'Catalogo', url:'/Catalogo/Arrurru.pdf'}]
+    { titulo: 'Catalogo', url:'/Catalogo/Belleza/Arrurru.pdf'}]
   },
   {
     id: 2,
     nombre: 'Hansaplast',
     categoria: ['belleza'],
     archivosPdf:  [ 
-    { titulo: 'Catalogo', url:'/Catalogo/Hansaplast.pdf'}]
+    { titulo: 'Catalogo', url:'/Catalogo/Belleza/Hansaplast.pdf'}]
   },
   {
     id: 3,
     nombre: 'Interbell',
     categoria: ['belleza'], 
     archivosPdf: [ 
-    { titulo: 'Catalogo', url:'/Catalogo/INTERBELL.pdf'}]
+    { titulo: 'Catalogo', url:'/Catalogo/Belleza/INTERBELL.pdf'}]
   },
   {
     id: 4,
     nombre: 'Intibon',
     categoria: ['belleza'],
     archivosPdf: [ 
-    { titulo: 'Catalogo', url:'/Catalogo/INTIBON.pdf'}]
+    { titulo: 'Catalogo', url:'/Catalogo/Belleza/INTIBON.pdf'}]
   },
   {
     id: 5,
     nombre: 'JGB',
     categoria: ['belleza'],
     archivosPdf: [ 
-    { titulo: 'Catalogo', url:'/Catalogo/JGB_Belleza.pdf'}]
-  },
-  {
-    id: 6,
-    nombre: 'Johnson',
-    categoria: ['belleza'],
-    archivosPdf: [ 
-    { titulo: 'Catalogo', url:'/Catalogo/Johnson.pdf'}]
+    { titulo: 'Catalogo', url:'/Catalogo/Belleza/JGB_Belleza.pdf'}]
   },
   {
     id: 7,
     nombre: 'Lander',
     categoria: ['belleza'],
     archivosPdf: [ 
-    { titulo: 'Catalogo', url:'/Catalogo/Lander.pdf'}]
+    { titulo: 'Catalogo', url:'/Catalogo/Belleza/Lander.pdf'}]
   },
   {
     id: 8,
     nombre: 'Ceresco',
     categoria: ['belleza'],
     archivosPdf: [ 
-    { titulo: 'Masglo', url:'/Catalogo/Masglo-1.pdf'},
-    { titulo: '', url:'/Catalogo/Masglo-2.pdf'},
-    { titulo: 'Admiss', url:'/Catalogo/Masglo-3.pdf'}]
+    { titulo: 'Masglo', url:'/Catalogo/Belleza/Masglo-1.pdf'},
+    { titulo: '', url:'/Catalogo/Belleza/Masglo-2.pdf'},
+    { titulo: 'Admiss', url:'/Catalogo/Belleza/Masglo-3.pdf'}]
   },
   {
     id: 9,
     nombre: 'Nivea',
     categoria: ['belleza'],
     archivosPdf: [ 
-    { titulo: 'Catalogo', url:'/Catalogo/Nivea.pdf'}]
+    { titulo: 'Catalogo', url:'/Catalogo/Belleza/Nivea.pdf'}]
   },
   {
     id: 10,
     nombre: 'Prebel',
     categoria: ['belleza'],
     archivosPdf: [ 
-    { titulo: 'Catalogo', url:'/Catalogo/PREBEL.pdf'}]
+    { titulo: 'Catalogo', url:'/Catalogo/Belleza/PREBEL.pdf'}]
   },
   {
     id: 11,
     nombre: 'Intibon',
     categoria: ['belleza'],
     archivosPdf: [ 
-    { titulo: 'Catalogo', url:'/Catalogo/INTIBON.pdf'}]
+    { titulo: 'Catalogo', url:'/Catalogo/Belleza/INTIBON.pdf'}]
   },
   {
     id: 12,
     nombre: 'Pronabell',
     categoria: ['belleza'],
     archivosPdf: [ 
-    { titulo: 'Catalogo', url:'/Catalogo/PRONABELL.pdf'}]
+    { titulo: 'Catalogo', url:'/Catalogo/Belleza/PRONABELL.pdf'}]
   },
   {
     id: 13,
     nombre: 'Recamier',
     categoria: ['belleza'],
     archivosPdf: [ 
-    { titulo: 'Catalogo', url:'/Catalogo/Recamier.pdf'}]
+    { titulo: 'Catalogo', url:'/Catalogo/Belleza/Recamier.pdf'}]
   },
   {
     id: 14,
     nombre: 'Roa',
     categoria: ['belleza'],
     archivosPdf: [ 
-    { titulo: 'Catalogo', url:'/Catalogo/Roa.pdf'}]
+    { titulo: 'Catalogo', url:'/Catalogo/Belleza/Roa.pdf'}]
   },
   {
     id: 15,
     nombre: 'Salome',
     categoria: ['belleza'],
     archivosPdf: [ 
-    { titulo: 'Catalogo-1', url:'/Catalogo/SALOME.pdf'},
-    { titulo: 'Catalogo-2', url:'/Catalogo/SALOME_SOB.pdf'}]
+    { titulo: 'Catalogo-1', url:'/Catalogo/Belleza/SALOME.pdf'},
+    { titulo: 'Catalogo-2', url:'/Catalogo/Belleza/SALOME_SOB.pdf'}]
   },
 ];
 

fe5fde00c1f537b269f03d631826ce8c90fe3517
JoseMontenegro2005 <josemontenegro@unicomfacauca.edu.co>
2025-10-26 20:53:37 -0500

    Version Final (3.0 ahora tiene mas cosas y el drogueria esta online)

--- src/data.js
+++ src/data.js
@@ -2,159 +2,267 @@ export const catalogos = [
   {
     id: 1,
     nombre: 'Arrurru',
-    categoria: ['belleza'], 
+    categoria: ['belleza','tradicional','drogueria'], 
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Belleza/Arrurru.pdf'}]
+    { titulo: '', url:'/Catalogo/Arrurru.pdf'}]
   },
   {
     id: 2,
-    nombre: 'Hansaplast',
-    categoria: ['belleza'],
+    nombre: 'DUO - Hansaplast',
+    categoria: ['belleza','tradicional','drogueria'],
     archivosPdf:  [ 
-    { titulo: '', url:'/Catalogo/Belleza/Hansaplast.pdf'}]
+    { titulo: '', url:'/Catalogo/Hansaplast.pdf'}]
   },
   {
     id: 3,
     nombre: 'Interbell',
-    categoria: ['belleza'], 
+    categoria: ['belleza','drogueria'], 
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Belleza/INTERBELL.pdf'}]
+    { titulo: '', url:'/Catalogo/INTERBELL.pdf'}]
   },
   {
     id: 4,
     nombre: 'Intibon',
-    categoria: ['belleza'],
+    categoria: ['belleza','tradicional','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Belleza/INTIBON.pdf'}]
+    { titulo: '', url:'/Catalogo/INTIBON.pdf'}]
   },
   {
     id: 5,
     nombre: 'JGB',
     categoria: ['belleza'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Belleza/JGB_Belleza.pdf'}]
+    { titulo: '', url:'/Catalogo/JGB_Belleza.pdf'}]
   },
   {
     id: 7,
     nombre: 'Lander',
-    categoria: ['belleza'],
+    categoria: ['belleza','tradicional','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Belleza/LANDER.pdf'}]
+    { titulo: '', url:'/Catalogo/LANDER.pdf'}]
   },
   {
     id: 8,
     nombre: 'Ceresco',
-    categoria: ['belleza'],
+    categoria: ['belleza','tradicional','drogueria'],
     archivosPdf: [ 
-    { titulo: 'Masglo', url:'/Catalogo/Belleza/Masglo-1.pdf'},
-    { titulo: '', url:'/Catalogo/Belleza/Masglo-2.pdf'},
-    { titulo: 'Admiss', url:'/Catalogo/Belleza/Masglo-3.pdf'}]
+    { titulo: 'Masglo', url:'/Catalogo/Masglo-1.pdf'},
+    { titulo: '', url:'/Catalogo/Masglo-2.pdf'},
+    { titulo: 'Admiss', url:'/Catalogo/Masglo-3.pdf'}]
   },
   {
     id: 9,
     nombre: 'Nivea',
-    categoria: ['belleza'],
+    categoria: ['belleza','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Belleza/Nivea.pdf'}]
+    { titulo: '', url:'/Catalogo/Nivea.pdf'}]
   },
   {
     id: 10,
     nombre: 'Prebel',
-    categoria: ['belleza'],
+    categoria: ['belleza','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Belleza/PREBEL.pdf'}]
+    { titulo: '', url:'/Catalogo/PREBEL.pdf'}]
   },
-  {
-    id: 11,
-    nombre: 'Intibon',
-    categoria: ['belleza'],
-    archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Belleza/INTIBON.pdf'}]
-  },
+  // {
+  //   id: 11,
+  //   nombre: 'Intibon',
+  //   categoria: ['belleza','drogueria'],
+  //   archivosPdf: [ 
+  //   { titulo: '', url:'/Catalogo/INTIBON.pdf'}]
+  // },
   {
     id: 12,
     nombre: 'Pronabell',
-    categoria: ['belleza'],
+    categoria: ['belleza','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Belleza/PRONABELL.pdf'}]
+    { titulo: '', url:'/Catalogo/PRONABELL.pdf'}]
   },
   {
     id: 13,
     nombre: 'Recamier',
-    categoria: ['belleza'],
+    categoria: ['belleza','tradicional','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Belleza/Recamier.pdf'}]
+    { titulo: '', url:'/Catalogo/RECAMIER.pdf'}]
   },
   {
     id: 14,
     nombre: 'Roa',
-    categoria: ['belleza'],
+    categoria: ['belleza','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Belleza/Roa.pdf'}]
+    { titulo: '', url:'/Catalogo/Roa.pdf'}]
   },
   {
     id: 15,
     nombre: 'Salome',
-    categoria: ['belleza'],
+    categoria: ['belleza','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Belleza/SALOME.pdf'},
-    { titulo: '', url:'/Catalogo/Belleza/SALOME_SOB.pdf'}]
+    { titulo: '', url:'/Catalogo/SALOME.pdf'},
+    { titulo: '', url:'/Catalogo/SALOME_SOB.pdf'}]
   },
   {
     id: 16,
     nombre: 'Almipro',
-    categoria: ['tradicional'],
+    categoria: ['tradicional','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Tradicional/ALMIPRO.pdf'}]
+    { titulo: '', url:'/Catalogo/ALMIPRO.pdf'}]
   },
   {
     id: 17,
     nombre: 'Bayer',
-    categoria: ['tradicional'],
+    categoria: ['tradicional','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Tradicional/BAYER.pdf'}]
+    { titulo: '', url:'/Catalogo/BAYER.pdf'}]
   },
   {
     id: 18,
     nombre: 'Begut',
-    categoria: ['tradicional'],
+    categoria: ['tradicional','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Tradicional/BEGUT.pdf'}]
+    { titulo: '', url:'/Catalogo/BEGUT.pdf'}]
   },
   {
     id: 19,
     nombre: 'Colgate',
-    categoria: ['tradicional'],
+    categoria: ['tradicional','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Tradicional/COLGATE.pdf'}]
+    { titulo: '', url:'/Catalogo/COLGATE.pdf'}]
   },
   {
     id: 20,
     nombre: 'Dromatic',
-    categoria: ['tradicional'],
+    categoria: ['tradicional','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Tradicional/DROMATIC.pdf'}]
+    { titulo: '', url:'/Catalogo/DROMATIC.pdf'}]
   },
   {
     id: 21,
     nombre: 'Electrolit',
-    categoria: ['tradicional'],
+    categoria: ['tradicional','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Tradicional/Electrolit.pdf'}]
+    { titulo: '', url:'/Catalogo/Electrolit.pdf'}]
   },
   {
     id: 22,
     nombre: 'JGB',
-    categoria: ['tradicional'],
+    categoria: ['tradicional','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Tradicional/JGB.pdf'}]
+    { titulo: '', url:'/Catalogo/JGB.pdf'}]
   },
   {
     id: 23,
     nombre: 'Osa',
-    categoria: ['tradicional'],
+    categoria: ['tradicional','drogueria'],
     archivosPdf: [ 
-    { titulo: '', url:'/Catalogo/Tradicional/OSA.pdf'}]
+    { titulo: '', url:'/Catalogo/OSA.pdf'}]
+  },
+  {
+    id: 24,
+    nombre: 'DKT',
+    categoria: ['belleza','tradicional','drogueria'],
+    archivosPdf: [ 
+    { titulo: '', url:'/Catalogo/DKT.pdf'},
+  //   { titulo: '', url:'/Catalogo/'}
+  ]
+  },
+  {
+    id: 25,
+    nombre: 'D´Mujer',
+    categoria: ['belleza','tradicional','drogueria'],
+    archivosPdf: [ 
+    { titulo: '', url:'/Catalogo/DMUJER.pdf'},
+  //   { titulo: '', url:'/Catalogo/'}
+  ]
+  },
+  {
+    id: 26,
+    nombre: 'Angela',
+    categoria: ['belleza','tradicional','drogueria'],
+    archivosPdf: [ 
+    { titulo: '', url:'/Catalogo/ANGELA.pdf'},
+  //   { titulo: '', url:'/Catalogo/'}
+  ]
+  },
+  {
+    id: 27,
+    nombre: 'Unilever',
+    categoria: ['belleza','tradicional','drogueria'],
+    archivosPdf: [ 
+    { titulo: '', url:'/Catalogo/UNILEVER.pdf'},
+  //   { titulo: '', url:'/Catalogo/'}
+  ]
+  },
+  {
+    id: 28,
+    nombre: 'Henkel',
+    categoria: ['belleza','tradicional','drogueria'],
+    archivosPdf: [ 
+    { titulo: '', url:'/Catalogo/HENKEL.pdf'},
+  //   { titulo: '', url:'/Catalogo/'}
+  ]
+  },
+  {
+    id: 29,
+    nombre: 'San Jorge',
+    categoria: ['belleza','tradicional','drogueria'],
+    archivosPdf: [ 
+    { titulo: '', url:'/Catalogo/SANJORGE.pdf'},
+  //   { titulo: '', url:'/Catalogo/'}
+  ]
+  },
+  {
+    id: 30,
+    nombre: 'Sonrident',
+    categoria: ['tradicional','drogueria'],
+    archivosPdf: [ 
+    { titulo: '', url:'/Catalogo/SONRIDENT.pdf'},
+  //   { titulo: '', url:'/Catalogo/'}
+  ]
+  },
+  {
+    id: 31,
+    nombre: 'Ecar',
+    categoria: ['drogueria'],
+    archivosPdf: [ 
+    { titulo: '', url:'/Catalogo/ECAR.pdf'},
+  //   { titulo: '', url:'/Catalogo/'}
+  ]
+  },
+  {
+    id: 32,
+    nombre: 'Eucerin',
+    categoria: ['drogueria','belleza','tradicional'],
+    archivosPdf: [ 
+    { titulo: '', url:'/Catalogo/EUCERIN.pdf'},
+  //   { titulo: '', url:'/Catalogo/'}
+  ]
+  },
+  {
+    id: 33,
+    nombre: 'Proquident',
+    categoria: ['drogueria','tradicional'],
+    archivosPdf: [ 
+    { titulo: '', url:'/Catalogo/PROQUIDENT.pdf'},
+  //   { titulo: '', url:'/Catalogo/'}
+  ]
+  },
+  {
+    id: 34,
+    nombre: 'Genfar',
+    categoria: ['drogueria'],
+    archivosPdf: [ 
+    { titulo: '', url:'/Catalogo/GENFAR'},
+  //   { titulo: '', url:'/Catalogo/'}
+  ]
+  },
+  {
+    id: 35,
+    nombre: 'Procaps',
+    categoria: ['drogueria'],
+    archivosPdf: [ 
+    { titulo: '', url:'/Catalogo/PROCAPS'},
+  //   { titulo: '', url:'/Catalogo/'}
+  ]
   },
   // {
   //   id: ,
@@ -162,9 +270,19 @@ export const catalogos = [
   //   categoria: [''],
   //   archivosPdf: [ 
   //   { titulo: '', url:'/Catalogo/'},
-  // //   { titulo: '', url:'/Catalogo/'}]
+  // //   { titulo: '', url:'/Catalogo/'}
+  // ]
   // },
-
+  // {
+  //   id: ,
+  //   nombre: '',
+  //   categoria: [''],
+  //   archivosPdf: [ 
+  //   { titulo: '', url:'/Catalogo/'},
+  // //   { titulo: '', url:'/Catalogo/'}
+  // ]
+  // },
+  
 
 ];
 
